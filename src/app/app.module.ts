7958b60f2d8bb72ad9cde98de375ace804376a55
Jhonatan Freitas <itjhonatan01@gmail.com>
2019-04-02 16:39:31 -0300

    version 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { HeroService } from '../providers/hero-service/hero-service';
+import { HttpModule } from '@angular/http';
+import { DescriptionPage } from '../pages/description/description';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +27,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    HeroService
   ]
 })
 export class AppModule {}
